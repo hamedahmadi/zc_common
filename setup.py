021e513d2457472fe27f9ee7a7ad13f0e23c1b5b
Hamed Ahmadi <hamed@zerocater.com>
2016-04-29 11:47:09 -0700

    adding remote resource module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='zc_common',
-      version='0.0.2',
+      version='0.0.3',
       description="A collection of Python utils",
       long_description='',
       keywords='zerocater python util',
@@ -10,7 +10,15 @@ setup(name='zc_common',
       url='https://github.com/ZeroCater/zc_common',
       download_url='https://github.com/ZeroCater/zc_common/tarball/0.0.2',
       license='MIT',
-      packages=['zc_common'],
+      packages=[
+          'zc_common',
+          'remote_resource',
+      ],
+      install_requires=[
+          'django',
+          'djangorestframework>=3.1.0',
+          'djangorestframework-jsonapi>=2.0.0b2'
+      ],
       classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
